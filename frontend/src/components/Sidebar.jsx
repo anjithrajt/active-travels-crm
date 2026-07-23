a135580943f3db18a0f5a82e6735163602465ecc
Anjith Raj <anjithanjith2540@gmail.com>
2026-07-23 23:55:30 +0530

    Redesigned sidebar with modern SaaS UI

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,15 @@
-import { Link } from "react-router-dom";
+import {
+  LayoutDashboard,
+  Users,
+  UserPlus,
+  CalendarClock,
+  FileText,
+  Plane,
+  FolderOpen,
+  Bell,
+  LogOut,
+  PlaneTakeoff,
+} from "lucide-react";
 
 function Sidebar() {
 const logout = () => {
@@ -6,7 +17,26 @@ const logout = () => {
   window.location.href = "/";
 };
   return (
-    <div className="w-64 bg-slate-900 text-white min-h-screen p-5">
+    <div className="w-72 min-h-screen bg-slate-900 text-white flex flex-col shadow-2xl">
+      <div className="p-6 border-b border-slate-700">
+    <div className="flex items-center gap-3">
+
+        <div className="bg-blue-600 p-3 rounded-xl">
+            <PlaneTakeoff size={28}/>
+        </div>
+
+        <div>
+            <h1 className="text-xl font-bold">
+                Active Travels
+            </h1>
+
+            <p className="text-slate-400 text-sm">
+                CRM Dashboard
+            </p>
+        </div>
+
+    </div>
+</div>
 
       <h1 className="text-2xl font-bold mb-8">
         Active Travels CRM
@@ -14,13 +44,34 @@ const logout = () => {
 
       <nav className="flex flex-col gap-4">
 
-        <Link to="/">Dashboard</Link>
-
-        <Link to="/customers">Customers</Link>
-
-        <Link to="/leads">Leads</Link>
-
-        <Link to="/followups">Follow Ups</Link>
+        <Link
+    to="/dashboard"
+    className="flex items-center gap-3 px-4 py-3 rounded-xl hover:bg-slate-800 transition-all duration-300"
+>
+    <LayoutDashboard size={20}/>
+    Dashboard
+</Link>
+        <Link
+    to="/customers"
+    className="flex items-center gap-3 px-4 py-3 rounded-xl hover:bg-slate-800 transition-all duration-300"
+>
+    <Users size={20}/>
+    Customers
+</Link>
+        <Link
+    to="/leads"
+    className="flex items-center gap-3 px-4 py-3 rounded-xl hover:bg-slate-800 transition-all duration-300"
+>
+    <UserPlus size={20}/>
+    Leads</Link>
+        <Link
+    to="/followups"
+    className="flex items-center gap-3 px-4 py-3 rounded-xl hover:bg-slate-800 transition-all duration-300"
+>
+    <CalendarClock size={20}/>
+    Follow Ups
+</Link>
+        <Link ></Link>
 
         <Link to="/documents">Documents</Link>
         <Link to="/visa-applications">
@@ -41,6 +92,7 @@ const logout = () => {
       </nav>
 
     </div>
+    
   );
 }
 
